d5b57d376b42249e941d12e227dc94d73de096d2
Guillermo <diazgbs@gmail.com>
2025-03-28 11:30:10 -0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full lg:w-6xl sm-w-full m-auto">
+      <div className="bg-slate-700 w-full mt-3 p-6 rounded-t-md text-slate-100">
+        <div>guillermods.com</div>
+      </div>
+      <div className="bg-white p-3 shadow">
+        <h2 className="font-bold">Spring Boot and React MUI - Table Example</h2>
+      </div>
+      <div className="w-full bg-slate-100 p-3 h-100">
+        Examples
+      </div>
+    </div>
+  );
+}
+
+export default App;
