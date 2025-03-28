541c46ec98a19ee08cb5ca57e4e3aa970fa05803
gdiazs <diazgbs@gmail.com>
2025-03-28 14:39:04 -0600

    Postgresql and h2

--- backend/src/main/java/com/guillermods/backend/users/User.java
+++ backend/src/main/java/com/guillermods/backend/users/User.java
@@ -3,28 +3,37 @@ package com.guillermods.backend.users;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 
 @Data
-@Table("USERS")
+@Table( name = "users")
 public class User {
 
     @Id
+    @Column("email")
     private String email;
 
+    @Column("username")
     private String username;
 
+    @Column("first_name")
     private String firstName;
 
+    @Column("last_name")
     private String lastName;
 
+    @Column("created_ts")
     private LocalDateTime createdTs;
 
+    @Column("updated_ts")
     private LocalDateTime updatedTs;
 
+    @Column("deleted_ts")
     private LocalDateTime deletedTs;
 
+    @Column("active")
     private boolean active;
 }
